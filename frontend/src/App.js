864a3f1a4b492623862091b090a5634eaa8167b3
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2022-01-20 03:25:25 +0900

    feat: route 변경시 scroll to top

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'index.css';
 import Splash from 'pages/Splash';
 import Main from 'pages/Main';
 import Search from 'pages/Search';
+import ScrollToTop from 'utils/ScrollToTop';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Splash />}></Route>
-        <Route path="/main" element={<Main />}></Route>
-        <Route path="/art/:category" element={<Search />}></Route>
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Splash />}></Route>
+          <Route path="/main" element={<Main />}></Route>
+          <Route path="/art/:category" element={<Search />}></Route>
+        </Routes>
+      </ScrollToTop>
     </Router>
   );
 }
